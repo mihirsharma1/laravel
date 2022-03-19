d58f0a835024e3f984c18d7b17255495cb56ed9d
mihirsharma1 <mihirsharma2509@gmail.com>
2022-03-19 20:38:34 +0530

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.dashboard')
+@section('title', 'EVENTS || SERVICES')
+@section('content')
+<div class="p-6">
+    <div class="border w-full">
+        dfgsdgfsdfg
+    </div>
+    <div class="border w-full">
+        asgsdfgsdfgsdf
+    </div>
+</div>
+@endsection
